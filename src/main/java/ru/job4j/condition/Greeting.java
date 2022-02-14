e2c0429ec694b95705e1b9881bf3f18ed58cd073
nikisha-script <nikisha_script@mail.ru>
2022-02-14 21:42:48 +0300

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Greeting {
+
+    public static void main(String[] args) {
+        String idea = "I like java!";
+        System.out.println(idea);
+        int year = 2022;
+        idea += year;
+        System.out.println(idea);
+    }
+
+}
