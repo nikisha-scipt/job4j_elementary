a2f9d69b39cfe66e2b7eb3a302ca8fad3421a1cb
nikisha-script <nikisha_script@mail.ru>
2022-02-15 08:37:21 +0300

    3. Арифметические операции. [#185 #264450] (Исправление)

--- src/main/java/ru/job4j/calculate/Calculator.java
+++ src/main/java/ru/job4j/calculate/Calculator.java
@@ -11,9 +11,9 @@ public class Calculator {
         int four = 4;
         int five = 5;
 
-        int sixDivTwo = six + two;
-        int fiveMinusTwo = five + two;
-        int fourTimeTwo = four + two;
+        int sixDivTwo = six / two;
+        int fiveMinusTwo = five - two;
+        int fourTimeTwo = four * two;
 
         System.out.println(sixDivTwo);
         System.out.println(fiveMinusTwo);
