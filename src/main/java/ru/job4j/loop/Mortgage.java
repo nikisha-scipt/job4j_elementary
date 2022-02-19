c339584eaaf67da3866f477b9e6b52864bf87d9b
nikisha-script <nikisha_script@mail.ru>
2022-02-19 14:19:32 +0300

    5.7. Ипотека [#156315]

---
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+
+    public static int year(double amount, int salary, double percent) {
+        int year = 0;
+        double sum = (amount + (amount * (percent / 100)));
+        while (sum > 0) {
+            year++;
+            sum -= salary;
+        }
+        return year;
+    }
+
+}
