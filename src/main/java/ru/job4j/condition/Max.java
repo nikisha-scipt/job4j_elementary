e1a3da9a02bb31257c324d39db9ccb9c7b9c2ab3
nikisha-script <nikisha_script@mail.ru>
2022-02-26 08:02:31 +0300

    3. Перегрузить метод max для трех чисел [#122644] (reload)

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -8,12 +8,12 @@ public class Max {
     }
 
     public static int max(int first, int second, int thrid) {
-        return first > second  && first > thrid ? first : second > thrid ? second : thrid;
+        return first > second  && first > thrid ? first : Math.max(second, thrid);
     }
 
     public static int max(int first, int second, int thrid, int four) {
         int res = max(first, second, thrid);
-        return res > four ? res : four;
+        return Math.max(res, four);
     }
 
     public static void main(String[] args) {
